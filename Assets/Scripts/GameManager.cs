e2d38dc06395b4f2422f0d5d3ec7dff297357a36
Marko Kristic <marko.kristic@learnet.se>
2024-01-15 11:49:01 +0100

    BaseGame

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class PlayerData
+{
+    [Header("Dynamic Values")]
+    public int playerMoney = 0;
+    public float passiveGainTime = 100f; // Change this to 10 seconds
+    public GameObject clickEffect = null;
+    // ...
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager GlobalGameManager = null;
+
+    [Header("Static References")]
+    public PlayerData CurrentPlayerData = null;
+    public TextMeshProUGUI PlayerMoneyText = null;
+    public RectTransform buttonRectTransform = null;
+
+    public Button buttonToShake;
+    public float shakeDuration = 0.5f;
+    public float shakeMagnitude = 0.1f;
+
+
+    private void Update()
+    {
+        PlayerMoneyText.text = CurrentPlayerData.playerMoney.ToString() + "$";
+    }
+
+    private void Start()
+    {
+        // Start the coroutine for passive money gain
+        StartCoroutine(PassiveMoneyGain());
+    }
+
+    // Coroutine for passive money gain
+    IEnumerator PassiveMoneyGain()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(CurrentPlayerData.passiveGainTime);
+            CurrentPlayerData.playerMoney++; // Increase money
+        }
+    }
+
+    public void OnClick()
+    {
+        CurrentPlayerData.playerMoney++;
+        ShakeButton();
+    }
+
+    private void ShakeButton()
+    {
+        StartCoroutine(ShakeAnimation());
+    }
+
+    private IEnumerator ShakeAnimation()
+    {
+        Vector3 originalPosition = buttonToShake.transform.position;
+
+        float elapsed = 0.0f;
+
+        while (elapsed < shakeDuration)
+        {
+            float x = originalPosition.x + Random.Range(-1f, 1f) * shakeMagnitude;
+            float y = originalPosition.y + Random.Range(-5f, 5f) * shakeMagnitude;
+
+            buttonToShake.transform.position = new Vector3(x, y, originalPosition.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        buttonToShake.transform.position = originalPosition;
+    }
+}
+
